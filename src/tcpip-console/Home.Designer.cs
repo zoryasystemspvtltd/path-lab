9e417fef83d68888a33aae8942eab2316cf60ed1
zoryarajiv <rajiv.mandal@zorya.co.in>
2025-07-28 17:07:21 +0530

    Thread logic added

--- src/tcpip-console/Home.Designer.cs
+++ src/tcpip-console/Home.Designer.cs
@@ -28,18 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            panel1 = new Panel();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.ControlLightLight;
+            label1.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
+            label1.Location = new Point(152, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(415, 37);
+            label1.TabIndex = 0;
+            label1.Text = "Laboratory Information Center";
+            // 
+            // panel1
+            // 
+            panel1.Location = new Point(6, 59);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(782, 379);
+            panel1.TabIndex = 1;
             // 
             // Home
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.HighlightText;
             ClientSize = new Size(800, 450);
+            Controls.Add(panel1);
+            Controls.Add(label1);
             Name = "Home";
-            Text = "Form1";
+            Text = "TCP/IP LIS Console";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Panel panel1;
     }
 }
